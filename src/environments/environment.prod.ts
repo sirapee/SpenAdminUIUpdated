436125ec9a256e13583078b4ae29a06a788c189e
Tayo Aderiye <40259162+TayoAderiye@users.noreply.github.com>
2024-01-12 20:51:49 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   idleTimeInMinutes: 5,
   // baseUrl: 'http://sandbox-apigateway-alb-235993164.eu-west-1.elb.amazonaws.com/admin/api/v1'
-  baseUrl: 'https://apigateway.myspen.com',
+  baseUrl: 'https://apigateway.myspen.com/admin/api/v1',
   // TestUrl:'https://sandbox-apigateway.myspen.com/admin/api/v1'
 };
